380d69d5a64fc2f5e78901b117a1324e6b6f43ac
pashasport <123305807+pashasport@users.noreply.github.com>
2023-07-13 12:27:26 +0300

    check-hw-js-10

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import './style.css';
-import { Notify } from 'notiflix';
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 
-selector = document.querySelector('.breed-select');
-divCatInfo = document.querySelector('.cat-info');
-loader = document.querySelector('.loader');
-error = document.querySelector('.error');
+const ref = {
+  selector: document.querySelector('.breed-select'),
+  divCatInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+};
+const { selector, divCatInfo, loader, error } = ref;
 
 loader.classList.replace('loader', 'is-hidden');
 error.classList.add('is-hidden');
@@ -49,6 +51,7 @@ function onSelectBreed(event) {
 
 function onFetchError(error) {
   selector.classList.remove('is-hidden');
+
   loader.classList.replace('loader', 'is-hidden');
 
   Notify.failure(
